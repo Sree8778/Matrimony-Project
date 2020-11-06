d68a1c5be8a24dad739cd752b8f5c664773cd893
Sree8778 <69071120+Sree8778@users.noreply.github.com>
2020-11-06 09:13:51 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,184 @@
+<?php include_once("includes/basic_includes.php");?>
+<?php include_once("functions.php"); ?>
+
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Find Your Perfect Partner - Matrimony | About :: Matrimony
+</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<link href="css/bootstrap-3.1.1.min.css" rel='stylesheet' type='text/css' />
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<!-- Custom Theme files -->
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<link href='//fonts.googleapis.com/css?family=Oswald:300,400,700' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Ubuntu:300,400,500,700' rel='stylesheet' type='text/css'>
+<!----font-Awesome----->
+<link href="css/font-awesome.css" rel="stylesheet"> 
+<!----font-Awesome----->
+<script>
+$(document).ready(function(){
+    $(".dropdown").hover(            
+        function() {
+            $('.dropdown-menu', this).stop( true, true ).slideDown("fast");
+            $(this).toggleClass('open');        
+        },
+        function() {
+            $('.dropdown-menu', this).stop( true, true ).slideUp("fast");
+            $(this).toggleClass('open');       
+        }
+    );
+});
+</script>
+</head>
+<body>
+<!-- ============================  Navigation Start =========================== -->
+ <?php include_once("includes/navigation.php");?>
+<!-- ============================  Navigation End ============================ -->
+<div class="grid_3">
+  <div class="container">
+   <div class="breadcrumb1">
+     <ul>
+        <a href="index.php"><i class="fa fa-home home_1"></i></a>
+        <span class="divider">&nbsp;|&nbsp;</span>
+        <li class="current-page">About</li>
+     </ul>
+   </div>
+   <div class="about">
+   	  <div class="col-md-6 about_left">
+   	  	<img src="images/mar2.jpg" class="img-responsive" alt=""/>
+   	  </div>
+   	  <div class="col-md-6 about_right">
+   	  	<h1>About us</h1>
+   	  	<p>DBMS Project</p>
+   	  	<div class="accordation">
+		   <div class="jb-accordion-wrapper">
+				<div class="jb-accordion-title">Topic<button type="button" class="jb-accordion-button" data-toggle="collapse" data-target="#accordion-1-"><i class="fa fa-angle-down"> </i></button></div>
+				<p><!-- /.accordion-title -->
+				</p><div id="accordion-1-" class="jb-accordion-content collapse in" style="height: auto;">
+				<p>” Matrimonial System” is an online web portal that will help a user to find partners by their choices.It is the project where the admin is the super user to access all the activity , admin can only block the user, delete the user,update the user, view all users details,edit users partner etc. Matrimonial is the free project based on php where the user need to register first by entering some details that have been asked to fill up and then the users will have access to choose his/her partner based on his/her performance.
+
+Admin and users are the actors of the system.
+Online matrimonial project in PHP is a totally dynamic web portal and is manage by admin from backend. Users or the members should fill-up the form like enter personal details, enter family details, enter the partner performances etc so that they can logged in to the system and there in the dashboard they can see the list of all the other users with their names and details. the members or the users can send the invitation to the other users and if they accepts their invitation request you can message to each other. In the system there is the advance search where they can search simply by the features or Id etc. It contain the features like view the profile, registration,Log in ,Log out, Send invitation,Message them after the invitation has been accepted.</p>
+				</div>
+				
+		</div>
+   	  </div>
+   	  <div class="clearfix"> </div>
+   </div>
+  </div>
+</div>
+<div class="about_middle">
+	<div class="container">
+	  <h2>Happy Clients</h2>
+	  <div class="about_middle-grid1">
+		<div class="col-sm-6 testi_grid list-item-0">
+			<blockquote class="testi_grid_blockquote">
+				<figure class="featured-thumbnail">
+					<img src="images/Anu1.jfif" class="img-responsive" alt=""/>
+				</figure>
+				<div><a href="#">Hey</a>
+				<div class="clearfix"></div>
+				</div>
+			</blockquote>
+		    <small class="testi-meta"><span class="user">Anushka</span></small>
+		</div>
+		<div class="col-sm-6 testi_grid list-item-1">
+			<blockquote class="testi_grid_blockquote">
+				<figure class="featured-thumbnail">
+					<img src="images/Prabhas1.jpg" class="img-responsive" alt=""/>
+				</figure>
+				<div><a href="#">Hi Darling</a>
+				<div class="clearfix"></div>
+				</div>
+			</blockquote>
+			<small class="testi-meta1"><span class="user">Prabhas </span></small>
+		</div>
+		<div class="clearfix"> </div>
+	  </div>
+	  <div class="about_middle-grid2">
+		<div class="col-sm-6 testi_grid list-item-0">
+			<blockquote class="testi_grid_blockquote">
+				<figure class="featured-thumbnail">
+					<img src="images/Prabhas1.jpg" class="img-responsive" alt=""/>
+				</figure>
+				<div><a href="#">Shall we go for Dinner</a>
+				<div class="clearfix"></div>
+				</div>
+			</blockquote>
+		    <small class="testi-meta"><span class="user">Prabhas</span></small>
+		</div>
+		<div class="col-sm-6 testi_grid list-item-1">
+			<blockquote class="testi_grid_blockquote">
+				<figure class="featured-thumbnail">
+					<img src="images/Anu1.jfif" class="img-responsive" alt=""/>
+				</figure>
+				<div><a href="#">Sure</a>
+				<div class="clearfix"></div>
+				</div>
+			</blockquote>
+			<small class="testi-meta1"><span class="user">Anushka </span></small>
+		</div>
+		<div class="clearfix"> </div>
+	  </div>
+	</div>
+</div>
+<div class="about_bottom">
+	<div class="container">
+		<h3>Team</h3>
+	   <div class="col-md-3 about_grid1">
+		  <ul class="posts-grid our-team">
+			<li class="list-item-1">
+				<figure class="thumbnail_1 thumbnail">
+					<a href="#"><img src="images/srv3.jpg"  class="img-responsive" alt=""/></a>
+					
+			    </figure>
+			    <div class="desc">
+			    	<h4><a href="#">CH.Sree Ram Varma</a></h4>
+			    	<p>19BCE0034</p>
+			    </div>
+			 </li>
+	       </ul>
+	   </div>
+	   <div class="col-md-3 about_grid1">
+		  <ul class="posts-grid our-team">
+			<li class="list-item-1">
+				<figure class="thumbnail_1 thumbnail">
+					<a href="#"><img src="images/Gou3.jpeg"  class="img-responsive" alt=""/></a>
+					
+			    </figure>
+			    <div class="desc">
+			    	<h4><a href="#">Panjam Goutham Reddy </a></h4>
+			    	<p>19BCI0027</p>
+			    </div>
+			 </li>
+	       </ul>
+	   </div>
+	   <div class="col-md-3 about_grid1">
+		  <ul class="posts-grid our-team">
+			<li class="list-item-1">
+				<figure class="thumbnail_1 thumbnail">
+					<a href="#"><img src="images/Shu1.jpeg"  class="img-responsive" alt=""/></a>
+					
+			    </figure>
+			    <div class="desc">
+			    	<h4><a href="#">Bahuraju susheel kumar</a></h4>
+			    	<p>19BCI0061</p>
+			    </div>
+			 </li>
+	       </ul>
+	   </div>
+	   
+	   <div class="clearfix"> </div>
+	</div>
+</div>
+
+
+
+
+	
